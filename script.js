5fd9d5b153e4a853e818f8a964a788970a20072f
sabarish-max <sit23ee061@sairamtap.edu.in>
2024-12-14 18:04:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function scrl1()
+{
+    document.getElementById('home').scrollIntoView({behavior: 'smooth'});
+}
+function scrl4()
+{
+    document.getElementById('ccnn').scrollIntoView({behavior: 'smooth'});
+}
+function scrl3()
+{
+    document.getElementById('cnn1').scrollIntoView({behavior: 'smooth'});
+}
+function scrl2()
+{
+    document.getElementById('cvv1').scrollIntoView({behavior: 'smooth'});
+}
